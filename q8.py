22de7f42ce8f3cce49ab6bd38ac7be65effa45d6
ram <tram83594@gmail.com>
2021-12-10 11:01:45 +0530

    this is my eight commit

---
+++ q8.py
@@ -0,0 +1,13 @@
+a=["abhi","ram","khemu","lakhan"]
+i=-1
+b=0
+n=[]
+str1=""
+while(b<len(a)):
+    str1+=str(a[i])
+    str1+=" "
+    b=b+1
+    i=i-1
+n.append(str1)
+print(n)
+#list reverse program
